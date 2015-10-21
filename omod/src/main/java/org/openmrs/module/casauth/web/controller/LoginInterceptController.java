1437313963ce40787e6fb6c27e53682dbf3962ef
Saptarshi Purkayastha <sunbiz@gmail.com>
2015-10-21 11:58:09 -0400

    Fixed login.htm workflow for UI 2.x

--- omod/src/main/java/org/openmrs/module/casauth/web/controller/LoginInterceptController.java
+++ omod/src/main/java/org/openmrs/module/casauth/web/controller/LoginInterceptController.java
@@ -10,14 +10,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.openmrs.User;
 import org.openmrs.api.context.Context;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
 
-@Controller
-public class LoginInterceptController {
+public class LoginInterceptController extends AbstractController {
 	
-	@RequestMapping( { "/login.htm" })
-	public String redirectIntercepted(HttpServletRequest req, HttpServletResponse res) throws MalformedURLException,
+	@Override
+	public ModelAndView handleRequestInternal(HttpServletRequest req, HttpServletResponse res) throws MalformedURLException,
 	        IOException {
 		String casTicket = req.getParameter("casticket");
 		if (casTicket == null) {
@@ -25,7 +24,8 @@ public class LoginInterceptController {
 			if (autheticatedUser == null) {
 				String casLoginUrl = Context.getAdministrationService().getGlobalProperty("casauth.endpoint.login");
 				String casAppCode = Context.getAdministrationService().getGlobalProperty("casauth.endpoint.appcode");
-				return "redirect:" + casLoginUrl + "?cassvc=" + casAppCode + "&casurl=" + req.getRequestURL();
+				return new ModelAndView("redirect:" + casLoginUrl + "?cassvc=" + casAppCode + "&casurl="
+				        + req.getRequestURL());
 			}
 		} else {
 			String casValidateUrl = Context.getAdministrationService().getGlobalProperty("casauth.endpoint.validate");
@@ -36,18 +36,17 @@ public class LoginInterceptController {
 			conn.setRequestMethod("GET");
 			conn.setDoOutput(true);
 			BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-			String user = new String();
 			if (in.readLine().equals("yes")) {
 				String superuserUsername = Context.getAdministrationService()
 				        .getGlobalProperty("casauth.superuser.username");
-				String superuserPAssword = Context.getAdministrationService()
-				        .getGlobalProperty("casauth.superuser.password");
-				user = in.readLine();
-				Context.authenticate(superuserUsername, superuserPAssword);
+				String superuserPassword = Context.getAdministrationService().getGlobalProperty(
+				    "casauth.superuser.zpassword");
+				String user = in.readLine();
+				Context.authenticate(superuserUsername, superuserPassword);
 				Context.becomeUser(user);
 			}
 			in.close();
 		}
-		return "index";
+		return new ModelAndView("index");
 	}
 }
