ed09c4ec94319d3b4f42344879dd3dba70bdb8ad
Saptarshi Purkayastha <sunbiz@gmail.com>
2015-10-22 13:01:38 -0400

    Fixed logout from OpenMRS and CAS

---
+++ omod/src/main/java/org/openmrs/module/casauth/filter/LogoutInterceptFilter.java
@@ -0,0 +1,50 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0 + Health disclaimer. If a copy of the MPL was not distributed with
+ * this file, You can obtain one at http://license.openmrs.org
+ */
+package org.openmrs.module.casauth.filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.openmrs.api.context.Context;
+
+/**
+ * @author sunbiz
+ * 
+ */
+//TODO: verify if local logout has not thrown an exception
+//TODO: intercept the /session method DELETE call to deal with logout from REST client app
+public class LogoutInterceptFilter implements Filter {
+	
+	@Override
+	public void init(FilterConfig fc) throws ServletException {
+		
+	}
+	
+	@Override
+	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+		HttpServletRequest request = (HttpServletRequest) req;
+		String requestURI = request.getRequestURI();
+		if (requestURI.endsWith("/logout")) {
+			Context.logout();
+			HttpServletResponse httpResponse = (HttpServletResponse) res;
+			httpResponse.sendRedirect(Context.getAdministrationService().getGlobalProperty("casauth.endpoint.logout"));
+		} else {
+			chain.doFilter(req, res);
+		}
+	}
+	
+	@Override
+	public void destroy() {
+	}
+	
+}
